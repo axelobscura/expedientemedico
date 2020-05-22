59d2657a82df1c30c0be18b4e7eb50ea92937b42
Axel Obscura <axosar@gmail.com>
2020-05-22 11:37:44 -0500

    Primer commit

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+function About() {
+    return (
+        <div className="container">
+            <Head>
+                <title>EXPEDIENTE MÉDICO</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            About
+        </div>
+    )
+}
+
+export default About;
