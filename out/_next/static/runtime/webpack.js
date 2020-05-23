3ff666e5f314153ac7b39b8ebbaf190b7177e9a0
Axel Obscura <axosar@gmail.com>
2020-05-23 14:38:43 -0500

    Test

--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f3acc8bc6d92a6228528";
+/******/ 	var hotCurrentHash = "54302ddd38cdd938afa1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/contacto.js
+++ pages/contacto.js
@@ -6,7 +6,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-const Contacto = () => {
+const Contacto = (props) => {
     return (
         <>
             <Header />
